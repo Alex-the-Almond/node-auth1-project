be40bda8c9ab9055096a547e05021bdfdcb58e01
Alexander Aymond <lynxkhaos@gmail.com>
2022-07-25 22:52:55 -0500

    completed

--- api/users/users-router.js
+++ api/users/users-router.js
@@ -1,3 +1,6 @@
+const router = require('express').Router();
+const { restricted } = require('../auth/auth-middleware')
+const User = require('./users-model')
 // Require the `restricted` middleware from `auth-middleware.js`. You will need it here!
 
 
@@ -23,6 +26,14 @@
     "message": "You shall not pass!"
   }
  */
-
+router.get('/', restricted, async (req, res, next) => {
+  try {
+    const users = await User.find()
+    res.json(users)
+  } catch (err) {
+    next(err)
+  }
+});
 
 // Don't forget to add the router to the `exports` object so it can be required in other modules
+module.exports = router;
